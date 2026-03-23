d5efc57137b42f968e4b701549d0f1c7a47035a6
Erika Pacheco <enath6.25@gmail.com>
2026-03-22 18:33:16 -0600

    add GET method to MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function showMovies()
+    {
+        $movies = Movie ::all();
+        return response()->json($movies);
+    }
 }
