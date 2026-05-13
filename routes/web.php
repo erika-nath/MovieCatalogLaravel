4991888ca3a787822a06743065bee2bed6d7b5e6
Erika Pacheco <enath6.25@gmail.com>
2026-05-12 19:58:13 -0600

    se agrega ruta para metodo put

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/allmovies', [MovieController::class, 'showMovies'])->name('Allmovies');
 Route::post('/newmovie', [MovieController::class, 'strore'])->name('movieStore');
 Route::get('/movie/{id}', [MovieController::class, 'showById'])->name('movieById');
+Route::put('/movie/{id}', [MovieController::class, 'update']);
