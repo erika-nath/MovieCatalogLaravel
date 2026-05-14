3bffc0eb2506ba9f82dd593a13e2a63d43033c90
Erika Pacheco <enath6.25@gmail.com>
2026-05-14 07:50:41 -0600

    feat:se agrega metodo put y se agrega el Csr

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+        'newmovie',
+        'movie/*'
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
