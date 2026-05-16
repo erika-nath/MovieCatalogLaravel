66c93e77ffc99cd92ca9ad4fa1a7ea113ccee8ac
Erika Pacheco <enath6.25@gmail.com>
2026-05-16 09:23:27 -0600

    se agrega carpeta service y configuración de httpClient

---
+++ frontend/src/app/services/movie.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+private apiUrl = 'http://127.0.0.1:8000/api/movies';
+  constructor(private http: HttpClient) { }
+  getMovies(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
